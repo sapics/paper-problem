386571166341536e4e0f1d90a00a236efcea89a1
T. Nishino <gv.nishino@gmail.com>
2022-02-28 11:58:42 +0900

    Update index.test.ts

--- index.test.ts
+++ index.test.ts
@@ -1,5 +1,6 @@
 const paper = require("paper-jsdom-canvas");
 
 test("paper problem demo", () => {
-    new paper.Path("M 10 10 H 90 V 90 H 10 L 10 10")
+  paper.setup([1024, 1024])
+  new paper.Path("M 10 10 H 90 V 90 H 10 L 10 10")
 })
